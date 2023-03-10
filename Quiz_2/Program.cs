d6b1c6e743a03146a7fb2473c6ce63adb3d0c572
Khoi <110973167+marked01one@users.noreply.github.com>
2023-03-09 20:50:03 -0800

    Redid code for Question 2

--- Quiz_2/Program.cs
+++ Quiz_2/Program.cs
@@ -4,36 +4,43 @@
   {
     public static int Main(string[] args)
     {
-      
-      
-      Q2_b(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]));
+      Q2_a(Convert.ToInt32(args[0]));
+      Q2_b(Convert.ToInt32(args[1]), Convert.ToInt32(args[2]));
       Console.Write("\n\n");
-      Q2_c(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]));
-
-
-      
+      Q2_c(Convert.ToInt32(args[1]), Convert.ToInt32(args[2]));
       return 0;
     }
 
-    public static void Q1(int num)
-    {
-      Console.WriteLine(string.Format("\n\nThe number given: {0}", num));
+    // public static void Q1(int num)
+    // {
+    //   Console.WriteLine(string.Format("\n\nThe number given: {0}", num));
 
-      int sum = 0;
-      while (num > 0)
-      {
-        sum *= 10;
-        sum += num % 10;
-        num /= 10;
-      }
+    //   int sum = 0;
+    //   while (num > 0)
+    //   {
+    //     sum *= 10;
+    //     sum += num % 10;
+    //     num /= 10;
+    //   }
 
-      Console.WriteLine(string.Format("The number reversed: {0}", sum));
-      Console.WriteLine(string.Format("The number reversed times 3: {0}", sum * 3));
-    }
+    //   Console.WriteLine(string.Format("The number reversed: {0}", sum));
+    //   Console.WriteLine(string.Format("The number reversed times 3: {0}", sum * 3));
+    // }
 
-    public static void Q2_b(double min_ppl, double max_ppl)
+    // Question #2 redo only!
+    public static void Q2_a(double ppl)
     {
       // Helper factorial functions
+      double res = 1;
+      for (double i = 0; i < ppl; i++)
+      {
+        res = res * (365-i) / 365;
+      }
+      Console.WriteLine(string.Format("\nQ2\na)\nIn this case, the value for P will be: {0:N6}\n", res));
+    }
+    public static void Q2_b(double min_ppl, double max_ppl)
+    {
+      Console.Write("b)");
       for (double i = min_ppl; i <= max_ppl; i++)
       {
         if ((i-1) % 5 == 0) Console.Write("\n");
@@ -51,7 +58,7 @@
 
     public static void Q2_c(int min_ppl, int max_ppl)
     {
-      // Helper factorial functions
+      Console.WriteLine("c)");
       for (int i = min_ppl; i <= max_ppl; i++)
       {
         double res = 1;
@@ -60,7 +67,7 @@
         {
           res = res * (365-j) / 365;
         }
-        
+
         if ((1 - res).CompareTo(0.9) >= 0)
         {
           Console.WriteLine(string.Format("The probability will be higher than 0.9 at {0} people", i));
